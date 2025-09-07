1a197f3c6dd1ba1810469df737893f985fd06492
AdminWebHead✔️ <AdminWebHead@DIV>
2025-09-07 21:44:14 +0530

    1st Pull 1st Push > Div

--- src/Common_Package/Comon_Method.java
+++ src/Common_Package/Comon_Method.java
@@ -6,6 +6,8 @@ public class Comon_Method {
 	{
 		System.out.println("Added By Bony");
 		System.out.println("1st Pull By Bony");
+		System.out.println("this is Added by Div- 1st Pull 1st Push");
+
 
 	}
 }
